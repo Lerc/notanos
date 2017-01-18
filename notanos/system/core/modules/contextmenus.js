452aa319921cecb39e3611cc941cbde2fd182216
Neil Graham <lerc.sds@gmail.com>
2017-01-19 09:06:35 +1300

    Preload initial images for context menu

--- notanos/system/core/modules/contextmenus.js
+++ notanos/system/core/modules/contextmenus.js
@@ -6,19 +6,24 @@
 		"system/data/icons/default/actions/bundle-run.png",
 		"system/data/icons/default/actions/bundle-4.png",
 		"system/data/icons/default/actions/bundle-look-inside.png"];
-	
+
+	for (let i of default_images) {
+		var preload=new Image();
+		preload.src=i;
+	}
+
     var test_icon="system/data/icons/fontawesome/users.svg";
 	default_actions=["duck","snowflake","run","umbrella","look inside"];
 	function makeArcMenu(subFields,callback) {
 		menuStructure=API.makeDefaultmenu();
-		
+
 	  var result = document.createElement("div");
 	  var overlay = result;
 	  result.className="eventcaptureoverlay";
 	  var center=result.appendNew("div","arcmenu hidden");
       var subItems=center.appendNew("div","subitem");
 	  var menu = center.appendNew("ul","tip");
-	  var killed=false;      
+	  var killed=false;
 	  function killMenu() {
 			if (killed) return;
 			document.body.removeChild(result);
@@ -26,17 +31,17 @@
 		}
 		function closeMenu() {
 			center.addEventListener("transitionend",killMenu);
-			center.addClass("hidden");		
+			center.addClass("hidden");
 		}
-	  function overlayMouseDown(e) {	
+	  function overlayMouseDown(e) {
       closeMenu();
-      callback("canceled");     
+      callback("canceled");
 		}
 
     function stopEventPropagation(e) {
       e.stopPropagation();
     }
-        
+
 		function reportClick(e) {
 
 			//console.log("click ",e.target);
@@ -49,10 +54,10 @@
 			var item=e.currentTarget.parentNode;
 			var oldone=subItems.querySelector("ul:not(.hidden)");
 			if (oldone) oldone.addClass("hidden");
-			if (item.sublist) {                           
+			if (item.sublist) {
 					item.sublist.removeClass("hidden");
 			}
-			
+
 			/*
 	closeMenu();
 	if (callback) callback(e);
@@ -67,7 +72,7 @@
 			}
 		}
 		if (menuStructure.length !=5) console.log("Menu is wrong size!");
-		
+
 	  for (var i=0; i<5;i++) {
 			var menuEntry=menuStructure[i];
 			var item=menu.appendNew("li","slice");
@@ -84,12 +89,12 @@
 			}
 
 			var childList=menuEntry.subFields;
-			var sublistElement=subItems.appendNew("ul","sublist hidden")            
+			var sublistElement=subItems.appendNew("ul","sublist hidden")
 			if (childList) {
 				//var childList=[1,2,3,4,5, 6,7,8,9,10,11, 12,13,14,15,16];
 				for (var j=0; j<childList.length;j++) {
 					  var childEntry=childList[j];
-						var subItem=sublistElement.appendNew("li","subbutton"); 
+						var subItem=sublistElement.appendNew("li","subbutton");
 						subItem.innerHTML='<img src="'+childEntry.image+'">';
 						if (childEntry.caption) {
 							subItem.infoCaption=childEntry.caption;
@@ -100,7 +105,7 @@
 						}
 						subItem.addEventListener("mousedown",stopEventPropagation);
 						subItem.addEventListener("click",reportClick);
-					
+
 				}
 				item.sublist=sublistElement;
 			}
@@ -135,7 +140,7 @@
 							},
 							{image:"system/data/icons/default/actions/bundle-hack.png",  caption: "Hack!", response: "Hack"
 							},
-														
+
 					 ]
 	}
 	API.makeDefaultSubFields = function() {
@@ -144,13 +149,13 @@
                  [],
                  ["open","edit","view","hack"],
                  ["a","b","c","d"]
-              ];  
+              ];
     }
 	API.attachArcMenu = function(element,subFields,callback) {
 		 var overlay = makeArcMenu(subFields,callback);
 		 var bounds=element.getBoundingClientRect();
 		 var menu=overlay.querySelector(".arcmenu")
-		 var pos=menu.style;		 
+		 var pos=menu.style;
 		 pos.left=bounds.left+(bounds.width/2)+"px";
 		 pos.top=bounds.top+(bounds.height/2)+"px";
 		 pos.width="10px";
@@ -159,7 +164,7 @@
 		 getComputedStyle(menu).left;
 		 menu.removeClass("hidden");
 	}
-	
+
 	API._init_ = function(callback) {
 		var justPreCaching = makeArcMenu(API.makeDefaultmenu());
 		callback();
